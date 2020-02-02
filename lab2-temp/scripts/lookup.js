32d94cdd831502276b9ff7c8d114517ffefd5e49
Jacob Rohde <44655192+EskeletoInd@users.noreply.github.com>
2020-02-02 01:38:14 -0700

    Added functionality for the forecast

--- lab2-temp/scripts/lookup.js
+++ lab2-temp/scripts/lookup.js
@@ -23,23 +23,15 @@ function getWeather(city_id) {
   fetch(urlBuilder(city_id, "weather"))
   .then((resp) => resp.json())
   .then((current) => {
-    drawCurrentWeather(current);
+    console.log(current);
+    //drawCurrentWeather(current);
   })
   fetch(urlBuilder(city_id, "forecast"))
   .then((resp) => resp.json())
   .then((forecast) => {
-    //drawForecastWeather(forecast);
+    drawForecastWeather(forecast);
   })
 }
-
-//<div class="reviews">
-//  <div class="review">
-//    <div class="avatar">
-//      <img src="/images/georgeIII.jpg" alt="">
-//    </div>
-//    <h3 class="username">King George III</p>
-//    <p class="comment">Absolute terrible service. Ended slapping my drink out of my hand</p>
-//  </div>
 
 function drawCurrentWeather(current) {
   var holder = document.createElement("div");
@@ -61,20 +53,72 @@ function drawCurrentWeather(current) {
   document.getElementById("weatherResults").innerHTML = results;
 }
 
+function timeToString(time) {
+  // Input = "2020-02-02 09:00:00"
+  return time;
+}
+
+function degreeToDirection(degree) {
+  // Input = 178
+  var val = Math.floor((degree / 22.5) + 0.5);
+  var arr = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"];
+  return arr[(val % 16)];
+}
+
+function buildForecastRow(current_weather) {
+  var row = document.createElement("tr");
+
+  // Add the Icon
+  var cell = document.createElement("td");
+  var img = document.createElement("img");
+  img.src = "http://openweathermap.org/img/w/" + current_weather.weather[0].icon + ".png";
+  cell.appendChild(img);
+  row.appendChild(cell);
+
+  // Add the Time
+  var cell = document.createElement("td");
+  cell.innerHTML = timeToString(current_weather.dt_txt);
+  cell.style.textAlign = "center";
+  row.appendChild(cell);
+
+  // Add the Low
+  var cell = document.createElement("td");
+  cell.innerHTML = current_weather.main.temp_min;
+  row.appendChild(cell);
+
+  // Add the High
+  var cell = document.createElement("td");
+  cell.innerHTML = current_weather.main.temp_max;
+  row.appendChild(cell);
+
+  // Add the Humidity
+  var cell = document.createElement("td");
+  cell.innerHTML = current_weather.main.humidity;
+  row.appendChild(cell);
+
+  // Add the Cloudiness
+  var cell = document.createElement("td");
+  cell.innerHTML = current_weather.clouds.all;
+  row.appendChild(cell);
+
+  // Add the Wind Direction
+  var cell = document.createElement("td");
+  cell.innerHTML = degreeToDirection(current_weather.wind.deg);
+  row.appendChild(cell);
+
+  // Add the Wind Speed
+  var cell = document.createElement("td");
+  cell.innerHTML = current_weather.wind.speed;
+  row.appendChild(cell);
+
+  return row;
+}
+
 function drawForecastWeather(forecast) {
   console.log(forecast);
-  let results = "";
-  results += '<h2>Forecast of ' + forecast.name + "</h2>";
-  for (let i = 0; i < forecast.weather.length; i++) {
-    results += '<img src="http://openweathermap.org/img/w/' + current.weather[i].icon + '.png"/>';
+  let table = document.getElementById("forecastTable");
+  for (i = 0; i < forecast.list.length; i++) {
+    var row = buildForecastRow(forecast.list[i]);
+    table.appendChild(row);
   }
-  results += '<h2>' + current.main.temp + " &deg;F</h2>"
-  results += "<p>"
-  for (let i = 0; i < current.weather.length; i++) {
-    results += current.weather[i].description
-    if (i !== current.weather.length - 1)
-      results += ", "
-  }
-  results += "</p>";
-  document.getElementById("forecastResults").innerHTML = results;
 }
