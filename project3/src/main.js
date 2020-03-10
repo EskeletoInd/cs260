cba42448cef6641f597882405cabface1a92ec2a
jake <jrohde3@yahoo.com>
2020-03-10 14:07:38 -0600

    Additional work to Async functions

--- project3/src/main.js
+++ project3/src/main.js
@@ -10,22 +10,25 @@ let data = {
   getDeck(numberOfDecks = 1) {
     return new Promise((resolve, reject) => {
       fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=" + numberOfDecks)
-      .then((response) => {
-        return response.json();
-      }).then((response) => {
-        console.log(response);
-        this.deckID = response.deck_id;
-        this.remaining = response.remaining;
-      }).then(() => resolve())
-      .catch((e) => reject(e));
+        .then((response) => {
+          return response.json();
+        }).then((response) => {
+          console.log(response);
+          this.deckID = response.deck_id;
+          this.remaining = response.remaining;
+        }).then(() => resolve())
+        .catch((e) => reject(e));
     });
   },
   drawCard(count = 1) {
-    return new Promise((resolve, reject) => {
-      if (this.deckID === null || this.remaining === 0) {
-        this.getDeck();
-      }
-      console.log(this.deckID);
+    if (this.deckID === null || this.remaining === 0) {
+      return Promise.resolve(this.getDeck().then(Promise.resolve(this._getCard(count))));
+    } else {
+      return Promise.resolve(this._getCard(count));
+    }
+  },
+  _getCard(count) {
+    return new Promise(function(resolve, reject) {
       fetch("https://deckofcardsapi.com/api/deck/" + this.deckID + "/draw/?count=" + count)
         .then((response) => {
           response.text().then((text) => console.log(text));
