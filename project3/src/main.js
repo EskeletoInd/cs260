67a9eeef63c11a4d2270c63392eefb35c19adf7a
jake <jrohde3@yahoo.com>
2020-03-10 11:03:03 -0600

    Work on the blackjack page

--- project3/src/main.js
+++ project3/src/main.js
@@ -5,11 +5,41 @@ import router from './router'
 Vue.config.productionTip = false
 
 data = {
-
+  deckID: null,
+  remaining: 0,
+  getDeck(numberOfDecks = 1) {
+    fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=" + numberOfDecks)
+      .then(response) {
+        return response.json();
+      }.then(response) {
+        deckID = response.deck_id;
+        remaining = response.remaining;
+      }
+  },
+  drawCard(count = 1) {
+    if (deckID === null || remaining === 0) {
+      getDeck();
+    }
+    fetch("https://deckofcardsapi.com/api/deck/" + deckID + "/draw/?count=" + count)
+      .then(response) {
+        return response.json();
+      }.then(response) {
+        remaining = response.remaining;
+        return response.cards;
+      }
+  },
+  reshuffleDeck() {
+    fetch("https://deckofcardsapi.com/api/deck/" + deckID + "/shuffle/")
+      .then(response) {
+        return response.json();
+      }.then(response) {
+        remaining = response.remaining;
+      }
+  },
 }
 
 new Vue({
-  router,//http://deckofcardsapi.com/
+  router, //http://deckofcardsapi.com/
   data,
   render: h => h(App)
 }).$mount('#app')
--- project3/src/views/Blackjack.vue
+++ project3/src/views/Blackjack.vue
@@ -1,5 +1,61 @@
 <template>
-  <div class="blackjack">
-    <h1>This Page will be a game of Blackjack</h1>
-  </div>
+<div class="blackjack">
+  <h1>This Page will be a game of Blackjack</h1>
+</div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      playerHand: [],
+      dealerHand: [],
+    }
+  },
+  methods: {
+    playerDrawCard() {
+      playerHand.append(drawCard());
+      if (playerHand() > 21) {
+        // Player Bust!
+      }
+    },
+    playDealer() {
+      while (dealerScore() < 16) {
+        dealerHand.append(drawCard);
+      }
+      scoreGame();
+    },
+    scoreGame() {
+      if (dealerScore() > 21) {
+        // Dealer Bust
+      } else if (dealerScore() > playerScore()) {
+        // Dealer Win
+      } else if (dealerScore() === playerScore()) {
+        // Dealer Win by Tie
+      } else {
+        // Player Win
+      }
+    },
+    restartGame() {
+
+    },
+  },
+  computed: {
+    playerScore() {
+      let total = [0];
+      for (let index in playerHand) {
+        let value = playerHand[index].value;
+        if (value === "KING" || value === "QUEEN" || value === "JACK") {
+          total.map(x => x + 10);
+        } else if (value === "ACE") {
+
+        } else {
+          total.map(x => x += parseInt(value));
+        }
+      }
+      return total;
+    },
+    dealerScore() {},
+  }
+}
+</script>
